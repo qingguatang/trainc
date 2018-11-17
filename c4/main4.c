53bca6219c4e8ec8dd5f363e54af3d183e798885
bencode <bencode@163.com>
2018-11-17 19:29:20 +0800

    update

--- c4/main4.c
+++ c4/main4.c
@@ -1,36 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-// windows下怎么睡
-// 根据需要查google，或文档
-#include <unistd.h>
-
-
-char *get_animal();
 
 
 int main(void) {
-  srand(time(NULL));
-
-  for (int i = 0; i < 100; i++) {
-    char *animal = get_animal();
-    printf("\b\b\b\b%s", animal);
-    fflush(stdout);
-    usleep(10000);
-  }
+  printf("函数和指针、还是星座和生肖");
 }
-
-
-char *get_animal() {
-  static char *animals[] = {
-    "鼠", "牛", "虎", "兔", "龙", "蛇",
-    "马", "羊", "猴", "鸡", "狗", "猪"
-  };
-//  int rnd = rand();
-//  printf("%d", rnd);
-  int index = rand() % 12;
-  char *animal = animals[index];
-  return animal;
-}
-
