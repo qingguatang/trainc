e8eb40d511c39bac4f24e01252b84ddc6493b68b
bencode <bencode@163.com>
2018-11-17 16:41:06 +0800

    update

--- c4/main4.c
+++ c4/main4.c
@@ -1,6 +1,60 @@
 #include <stdio.h>
 
 
+char *get_zodiac(int year) {
+  static char *animals[] = {
+    "鼠", "牛", "虎", "兔", "龙", "蛇",
+    "马", "羊", "猴", "鸡", "狗", "猪"
+  };
+
+  // 1900
+  int index = (year - 1900) % 12;
+  char *zodiac = animals[index];
+
+  return zodiac;
+}
+
+char *get_sign(int month, int day) {
+  static char *signs[] = {
+    "水瓶座",
+    "双子座",
+    "牡羊座",
+    "金牛座",
+    "双子座",
+    "巨蟹座",
+    "狮子座",
+    "处女座",
+    "天秤座",
+    "天蝎座",
+    "射手座",
+    "摩羯座"
+  };
+
+  int sindex = month - 1;
+  if (day < 21) {
+    sindex--;
+    if (sindex == 0) {
+      sindex = 12;
+    }
+  }
+  char *sign = signs[sindex];
+  return sign;
+}
+
+
 int main(void) {
-  printf("无聊的C语言");
+  printf("请输入你的生日：");
+
+  int year;
+  int month;
+  int day;
+  scanf("%d %d %d", &year, &month, &day);
+  printf("你的生日为：%d年%d月%d日\n", year, month, day);
+
+  char *animal = get_zodiac(year);
+  printf("属%s", animal);
+
+  char *sign = get_sign(month, day);
+
+  printf(" %s", sign);
 }
