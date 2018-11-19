9de9f13964359d24f32eeb09639a5a2bdd71dcfe
bencode <bencode@163.com>
2018-11-19 17:14:53 +0800

    fixed

--- c4/main4.c
+++ c4/main4.c
@@ -1,34 +1,7 @@
 #include <stdio.h>
 
 
-// int
-// float
-// int*
-void swap(int *a, int *b) {
-  int t = *a;
-  *a = *b;
-  *b = t;
-}
-
-
-// int
-// float
-// int *
-void set(int *p) {
-//  printf("%p, 内容为：%d\n", p, *p);
-  *p = 100;
-}
-
-
 int main(void) {
-  printf("请输入两个数字：");
-  int a;
-  int b;
-  scanf("%d %d", &a, &b);
-
-  if (a > b){
-    swap(&a, &b);
-  }
-
-  printf("%d %d", a, b);
+  printf("函数和指针、还是星座和生肖");
 }
+
