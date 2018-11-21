09014a11d59e24feaf6da0c722a98945f84c2946
bencode <bencode@163.com>
2018-11-21 15:46:24 +0800

    辛苦啦

--- c5/sort.c
+++ c5/sort.c
@@ -15,11 +15,37 @@ void swap(int *list, int i, int j) {
   list[j] = t;
 }
 
-void sort(int *list, int n) {
+
+int int_comp(int a, int b) {
+  return a - b;
+}
+
+int remain_comp(int a, int b) {
+  return a % 10 - b % 10;
+}
+
+int reverse_comp(int a, int b) {
+  return b - a;
+}
+
+
+// int a
+// float a
+// long a
+
+// int a[100]
+// long a[100]
+
+// int *a;
+// char *a; 字符串
+
+// char *a[]  // 相当于 char *(a[])
+
+void sort(int *list, int n, int (*comp)(int, int)) {
   for (int i = 0; i < n; i++) {
     int min = i;
     for (int j = i + 1; j < n; j++) {
-      if (list[j] < list[min]) {
+      if (comp(list[j], list[min]) < 0) {
         min = j;
       }
     }
@@ -31,13 +57,21 @@ void sort(int *list, int n) {
 int main(void) {
   srand(time(NULL));
 
-  int n = 5 * 10000;
+  int n = 100 * 10000;
+//  int n = 10;
   int list[n];
   init_nums(list, n);
 
   int begin = clock();
-  sort(list, n);
+
+//  sort(list, n, reverse_comp);
+  qsort(list, n, sizeof(int), reverse_comp);
+
   int end = clock();
 
-  printf("cost %f s", 1.0 * (end - begin) / CLOCKS_PER_SEC);
+//  for (int i = 0; i < n; i++) {
+//    printf("%d ", list[i]);
+//  }
+
+  printf("\ncost %f s", 1.0 * (end - begin) / CLOCKS_PER_SEC);
 }
