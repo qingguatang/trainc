07878d5fe81e4d6eac5af25bd02dc96e0c2a9ed3
bencode <bencode@163.com>
2018-12-14 13:28:42 +0800

    add leapyear

---
+++ essential/README.md
@@ -0,0 +1,6 @@
+# C语言考试必会程序
+
+## 数
+
+- 素数判断 [prime.c](prime.c)
+- 闰年 [leap.c](leap.c)
---
+++ essential/leap.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * 判断某一年是否为闰年
+ * 闰年的规则为: 四年一闰，百年不闰，四百年一闰
+ * 比如 2016年是闰年，2020年是闰年，2100年不是闰年，但是2400年是闰年
+ */
+int is_leapyear(int year) {
+  return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+}
+
+int main(void) {
+  int year;
+
+  int years[] = {2016, 2020, 2100, 2400};
+  int size = sizeof(years) / sizeof(int);
+
+  for (int i = 0; i < size; i++) {
+    if (is_leapyear(years[i])) {
+      printf("%d年是闰年\n", years[i]);
+    } else {
+      printf("%d年不是闰年\n", years[i]);
+    }
+  }
+}
