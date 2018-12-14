ec6a86a3616bd18c5b140410a7a7f53ff02613a8
bencode <bencode@163.com>
2018-12-14 13:18:40 +0800

    docs

--- essential/prime.c
+++ essential/prime.c
@@ -2,6 +2,11 @@
 #include <math.h>
 
 
+/**
+ * 判断一个数是否为素数
+ * @param num  - 大于1的数字
+ * @return 返回1表示素数，0表示不是素数
+ */
 int is_prime(int num) {
   int n = (int)sqrt(num);
   for (int i = 2; i <= n; i++) {
