08f079e0bc4620234a6d85239d9f495da6361b2f
bencode <bencode@163.com>
2018-12-17 17:56:27 +0800

    update

--- c6/str1.c
+++ c6/str1.c
@@ -3,9 +3,14 @@
 
 
 int main(void) {
-  char s[] = "1234";
-  char s2[] = {'1','2','3','4','\0'};
-  printf("%d\n", sizeof(s));
-  printf("%d\n", sizeof(s2));
-  printf("%d\n",strlen(s));
+
+
+
+
+
+//  char s[] = "1234";
+//  char s2[] = {'1','2','3','4','\0'};
+//  printf("%d\n", sizeof(s));
+//  printf("%d\n", sizeof(s2));
+//  printf("%d\n",strlen(s));
 }
