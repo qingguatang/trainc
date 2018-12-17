e64f57721db1992039aeae34a938d67bb10108b5
bencode <bencode@163.com>
2018-12-17 17:55:43 +0800

    udpate

--- c6/array5.c
+++ c6/array5.c
@@ -2,7 +2,13 @@
 
 
 int main(void) {
-//  int a[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+  int a[3][4] = {
+    {1, 2,  3,  4},
+    {5, 6,  7,  8},
+    {9, 10, 11, 12}
+  };
+
+
 //  int len = sizeof(a) / sizeof(a[0]);
 //  printf("%d", len);
 //  for (int i = 0; i < len; i++) {
