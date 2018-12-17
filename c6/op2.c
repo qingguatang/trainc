d10107b38bfd0876106b3f8728847598bf19c3fc
bencode <bencode@163.com>
2018-12-17 17:00:46 +0800

    update

--- c6/op2.c
+++ c6/op2.c
@@ -4,13 +4,12 @@
 int main(void) {
   int i = 1, j = 2, k = 3;
   if (i++==1&&(++j==3||k++==3)) {
-//    printf("%d %d %d\n", i, j, k);
+    printf("%d %d %d\n", i, j, k);
   }
 
-
-  int m = 0;
-  if (m=1) {
-    printf("ok");
-  }
+//  int m = 0;
+//  if (m=1) {
+//    printf("ok");
+//  }
 }
 
